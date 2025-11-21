3a51cc4a6bcb74167c06ef75a4feed7bc00a1097
lucas <lucasmartiniquinteros@gmail.com>
2025-11-21 10:39:39 -0300

    tratamientos de nulos y columnas vacias

--- Parciales/Parcial1/Parcial1.py
+++ Parciales/Parcial1/Parcial1.py
@@ -8,7 +8,7 @@ CSV = "ventas-de-productores-de-glp.csv"
 
 
 # Carga y exploración inicial
-df = pd.read_csv(CSV)
+df = pd.read_csv(CSV, low_memory=False)
 
 print("\n=== SHAPE ===")
 print(df.shape)
@@ -70,5 +70,41 @@ precio_ordenado = precio_promedio.sort_values(ascending=False)
 print("\n=== PRECIO PROMEDIO POR MES ORDENADO DE MAYOR A MENOR ===")
 print(precio_ordenado)
 
+
+# Conversion de tipos columnas Object a tipado furte:
+##Columna vendedor
+df["Vendedor"] = df["Vendedor"].astype(str)
+## Columna de planta de carga
+df["Planta Carga"] = df["Planta Carga"].astype(str)
+## Columna de tipo de producto
+df["Tipo Producto"] = df["Tipo Producto"].astype(str)
+## Cuit de comprador to numeric - tiene cosas como 2046330 y 20-46330
+df["Cuit Comprador"] = df["Cuit Comprador"].astype(str)
+
+##Funcion para pasar todos los cuits a XX-XXXXXXX-x
+def formatear_cuit(cuit):
+    cuit = str(cuit).replace("-","")
+    if len(cuit) == 11:
+        return f"{cuit[:2]}-{cuit[2:10]}-{cuit[10]}"
+    return cuit  # Devuelve sin cambios si no tiene longitud válida
+## Aplicar la funcion a la columna
+df['Cuit Comprador'] = df['Cuit Comprador'].astype(str).apply(formatear_cuit)
+
+##continuamos actualizando las columnas tipo object: Comprador
+df["Comprador"] = df["Comprador"].astype(str)
+
+#Actividad comprador
+df["Actividad Comprador"] = df["Actividad Comprador"].astype(str)
+
+##Fecha data a datetime
+df["Fecha Data"] = pd.to_datetime(df["Fecha Data"])
+
+##Mostrar nuevos tipos
+print("\n=== DTYPES ===")
+print("Tratamiento de tipos de datos object:")
+print(df.dtypes)
+# corroboramos si hubo cambios respecto a nulos
+print("\n=== NULOS POR COLUMNA ===")
+print(df.isna().sum())
 
 
